47518292aed29d8c345d35ef6b8fb2504302e9ea
Matthew Carr <mdcarr941@gmail.com>
2019-02-18 10:55:13 -0500

    Worked on the Angular app.

--- banx-app/src/app/app.component.ts
+++ banx-app/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
 import { ProblemIndex } from '../../../lib/schema';
 
-declare const problemIndex: ProblemIndex;
+declare const problemIndexInitial: ProblemIndex;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'banx-app';
+export class AppComponent implements OnInit {
+  public title = 'banx-app';
+  public problemIndex$ = new BehaviorSubject(null);
+
+  ngOnInit() {
+    this.problemIndex$.next(problemIndexInitial);
+  }
 }
