45350cdd1287d870571aa2132416b2d6ee27a208
Matthew Carr <mdcarr941@gmail.com>
2019-03-10 15:41:41 -0400

    Created a script to make the symbolic links in `public` to the static content in `node_modules`.

---
+++ make_links.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Create links in `public` to static content in `node_modules`.
+set -e
+
+cd public
+ln -sf ../node_modules/bootstrap/dist bootstrap
+ln -sf ../node_modules/jquery/dist jquery
+ln -sf ../node_modules/mathjax mathjax
+ln -sf ../node_modules/popper.js/dist popper.js
