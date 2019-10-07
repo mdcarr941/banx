e90e36a3fced00948ccf2a51e44bdf758f1b8360
Matthew Carr <mdcarr941@gmail.com>
2019-10-07 00:00:49 -0400

    Started working on file system management code for the repositories.

---
+++ src/spec/repository.spec.js
@@ -0,0 +1,32 @@
+describe('Repository', function() {
+    const Repository = require('../bin/schema').Repository;
+    const fs = require('fs');
+    const path = require('path');
+    const repo = new Repository({name: 'RepositoryTests'});
+
+    async function exists(filePath) {
+        try {
+            await fs.promises.access(filePath, fs.constants.F_OK);
+            return true;
+        }
+        catch {
+            return false;
+        }
+    }
+
+    beforeAll(async function() {
+        if (await exists(repo.path)) {
+            await repo.rm('.');
+        }
+    });
+
+    it('should be instantiated', function() {
+        expect(repo).toBeTruthy();
+    });
+
+    it('should be able to create directories', async function() {
+        const sub = 'mkdirTest';
+        await repo.mkdir(sub);
+        expect(await exists(repo.fullPath(sub))).toBe(true);
+    });
+});
